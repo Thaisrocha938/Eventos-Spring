ceb504d04825a20ce7099a16901e7374dc2ee51e
Thaisrocha938 <thatath938@gmail.com>
2021-05-28 23:01:45 -0300

    primeiros testes

--- src/main/java/com/casal/demo/TesteSpringBootApplication.java
+++ src/main/java/com/casal/demo/TesteSpringBootApplication.java
@@ -2,8 +2,12 @@ package com.casal.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.casal.controllers"})
+@EntityScan(basePackages = { "com.casal.models" })
 public class TesteSpringBootApplication {
 
 	public static void main(String[] args) {
