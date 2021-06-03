4f254214ada8342470b9b26978db20b1eb63120c
Thaisrocha938 <thatath938@gmail.com>
2021-06-03 15:14:18 -0300

    primeiro commit

--- src/main/java/com/example/demo/models/Eventos.java
+++ src/main/java/com/example/demo/models/Eventos.java
@@ -1,15 +1,13 @@
-package models;
+package com.example.demo.models;
 
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
+@Table
 public class Eventos implements Serializable{
 
     private static final long serialVersionUID = 1L;
@@ -22,37 +20,43 @@ public class Eventos implements Serializable{
     private String data;
     private String horario;
 
-    
     public long getCodigo() {
         return codigo;
     }
+
     public void setCodigo(long codigo) {
         this.codigo = codigo;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getLocal() {
         return local;
     }
+
     public void setLocal(String local) {
         this.local = local;
     }
+
     public String getData() {
         return data;
     }
+
     public void setData(String data) {
         this.data = data;
     }
+
     public String getHorario() {
         return horario;
     }
+
     public void setHorario(String horario) {
         this.horario = horario;
     }
-
-    
 }
