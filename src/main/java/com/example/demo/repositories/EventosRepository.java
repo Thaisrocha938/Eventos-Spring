a8285b0bd11bc2ed45fb29f93739f043e28c04f9
Thais Rocha Costa <thatath938@gmail.com>
2021-08-03 14:53:30 -0300

    Alterar EventosRepository

--- src/main/java/com/example/demo/repositories/EventosRepository.java
+++ src/main/java/com/example/demo/repositories/EventosRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Eventos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventosRepository extends JpaRepository<Eventos, Long> {
 
 }
--- src/main/java/com/example/demo/repository/EventosRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.Eventos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventosRepository extends JpaRepository<Eventos, Long> {
-   Eventos findById(long id);
-}
